271c9160f14c467da7668e1001b858f9b4891616
sivanukg <sivamca09@gmail.com>
2021-05-27 11:07:22 +0530

    DEV-223 - added sysout in main method

--- demo_api/src/main/java/com/demo/api/Application.java
+++ demo_api/src/main/java/com/demo/api/Application.java
@@ -1,3 +1,13 @@
+/*************************************************************************************************
+ * Copyright (C) 2020   KAVYA, PVT LTD   legal@kavyaComapnay.com
+ * 
+ * This file is part of www.kavyaComapnay.com 
+ * All Rights Reserved.  Unauthorized copying of this file, via any medium is strictly prohibited
+ *
+ * We KAVYA COMPANY can not be copied in part or in full 
+ * and/or distributed without the express permission of kavyaComapnay
+**************************************************************************************************/
+
 package com.demo.api;
 
 import org.springframework.boot.SpringApplication;
@@ -11,6 +21,9 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableAutoConfiguration
 public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
+		System.out.println("This is my first Spring boot project");
+		
+		
 		SpringApplication.run(Application.class, args);
         }  
 
